63c94c4c448af2251f09329b9e89a8a4281339e3
MBcze <m_bradavka@utb.cz>
2024-12-09 20:32:45 +0100

    Settings page (About a DarkMode)

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonList, IonItem, IonLabel, IonToggle, IonSelect, IonSelectOption, IonButton, AlertController } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonList, IonItem, IonLabel, IonToggle, IonSelect, IonSelectOption, IonButton],
 })
 export class SettingsPage {
-  constructor() {}
+  constructor(private alertController: AlertController) {}
+
+  toggleDarkMode(event: any) {
+    document.body.classList.toggle('dark', event.detail.checked);
+  }
+
+  async showAbout() {
+    const alert = await this.alertController.create({
+      header: 'QuickShare',
+      subHeader: 'Version 1.0.0',
+      message: 'Michal Bradávka UTB',
+      buttons: ['OK'],
+      cssClass: 'about-alert'
+    });
+
+    await alert.present();
+  }
+
+  resetApp() {
+    console.log('App reset');
+  }
 }
